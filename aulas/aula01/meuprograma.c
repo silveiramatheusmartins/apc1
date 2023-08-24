2cf0f92118e429992fd90bb419e3e4454f102058
silveiramatheus <25529751-silveiramatheus@users.noreply.replit.com>
2023-08-24 00:46:17 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  // funcao principal
+  printf("ola pessoal\n");
+  printf("A raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
+
+
---
+++ praticas/pratica01/questao01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float nota_a1 = 5.5f;
+    float nota_a2 = 4.0f;
+
+  float media = 0.4 * nota_a1 + 0.6 * nota_a2;
+
+  printf("A media é %.2f\n", media);
+  
+
+    return 0;
+}
